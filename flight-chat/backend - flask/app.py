2f814b0d683fa85295a645ea6dc3670ede8978e5
Arda Harman <92380011+ecarmu@users.noreply.github.com>
2025-05-17 16:23:07 +0300

    Update app.py

--- flight-chat/backend - flask/app.py
+++ flight-chat/backend - flask/app.py
@@ -84,4 +84,4 @@ if __name__ == '__main__':
 
     init_db()
     #app.run(debug=True)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=True, allow_unsafe_werkzeug=True))
